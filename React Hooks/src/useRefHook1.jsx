f4d6c20dca303ce5c3bc31123413df51882e58dc
Manoj Kumar <119638620+manojLovesCoding@users.noreply.github.com>
2024-09-23 15:56:42 +0530

    commit in description
    useRef Hook will not re-render the component when the its variable named count's value changed.
    instead if u add useState then the count value will be changed when the count value will be changed it will re-render the component.Thats why we use useRef Hook when we dont want to re-render the component when the value is changed

---
+++ React Hooks/src/useRefHook1.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useRef, useState } from "react";
+import './App.css'
+
+function App() {
+
+    const [value, setValue] = useState(0)
+    const count = useRef(0)
+
+    useEffect(() => {
+        count.current = count.current + 1;
+    });
+
+    return (
+        <>
+            <button onClick={() => { setValue(prev => prev - 1) }}>-1</button>
+            <h1>{value}</h1>
+            <button onClick={() => { setValue(prev => prev + 1) }}>+1</button>
+
+            <h1>Render Count: {count.current}</h1>
+        </>
+    )
+}
+
+export default App
