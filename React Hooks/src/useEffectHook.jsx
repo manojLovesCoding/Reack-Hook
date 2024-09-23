e838d3cde253b1c0bb4640c82b2bbaee859ad832
manojLovesCoding <mkum9417@gmail.com>
2024-09-23 15:01:31 +0530

    useEffect-hook without dependency

---
+++ React Hooks/src/useEffectHook.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import './App.css'
+
+function App() {
+
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCount(count => count + 1)
+        }, 2000)
+    })
+
+    return (
+        <>
+            <h1>I've rendered {count} times</h1>
+        </>
+    )
+}
+
+export default App
