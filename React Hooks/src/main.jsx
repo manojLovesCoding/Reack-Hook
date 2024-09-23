98e34dfe89c1a4fa39db6a765439dc4995b055ee
manojLovesCoding <mkum9417@gmail.com>
2024-09-23 15:18:44 +0530

    useEffect Hook with array value dependcy

--- React Hooks/src/main.jsx
+++ React Hooks/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './useEffectHook2.jsx'
+import App from './useEffectHook3.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
---
+++ React Hooks/src/useEffectHook3.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import './App.css'
+
+function App() {
+
+    const [count, setCount] = useState(0)
+    const [name, setName] = useState("GreatStack")
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCount(count => count + 1)
+        }, 2000)
+    }, [count, name])
+
+    return (
+        <>
+            <h1>I've rendered {count} times</h1>
+        </>
+    )
+}
+
+export default App
